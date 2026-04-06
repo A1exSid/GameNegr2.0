40d2aa6e9df932ac502b920ed4103319dae34d92
A1exSid <alexsiboleg@gmail.com>
2026-04-06 19:24:33 +0700

    Demo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,14 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody2D rb;
 
+    Animator anim;    
+
     [SerializeField] float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,5 +21,14 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         rb.linearVelocity = new Vector2(moveX, moveY).normalized * speed;
+        if(rb.linearVelocity.magnitude > 0f)
+        {
+            anim.SetBool("Walks", true);
+        }
+        else
+        {
+            anim.SetBool("Walks", false);
+            
+        }
     }
 }
