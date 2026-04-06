aacb0d504d2d4bc053ab01f0599eb0db975c4ec9
A1exSid <alexsiboleg@gmail.com>
2026-04-06 18:36:35 +0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    [SerializeField] float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        rb.linearVelocity = new Vector2(moveX, moveY).normalized * speed;
+    }
+}
